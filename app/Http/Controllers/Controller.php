aa877c9f56c4ffed49b7279909c31827f964f5b3
moodrain <moerain@qq.com>
2020-06-17 17:35:14 +0800

    first commit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,7 +8,6 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Str;
 
 class Controller extends BaseController
 {

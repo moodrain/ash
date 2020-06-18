7cdb5e66475146a9d1bdd84b77a8bd17dcbecce3
moodrain <moerain@qq.com>
2020-06-18 15:38:05 +0800

    first commit

--- resources/views/layout/frame-aside.blade.php
+++ resources/views/layout/frame-aside.blade.php
@@ -1,7 +1,7 @@
-<el-menu-item index="dashboard" @click="$to('/')">Dashboard</el-menu-item>
+<el-menu-item index="dashboard" @click="$to('/', {}, true)">Dashboard</el-menu-item>
 
 <el-submenu index="post">
     <template slot="title">Post</template>
-    <el-menu-item index="post-list" @click="$to('/post/list')">Post List</el-menu-item>
-    <el-menu-item index="post-edit" @click="$to('/post/edit')">Post Edit</el-menu-item>
+    <el-menu-item index="post-list" @click="$to('/post/list', {}, true)">Post List</el-menu-item>
+    <el-menu-item index="post-edit" @click="$to('/post/edit', {}, true)">Post Edit</el-menu-item>
 </el-submenu>

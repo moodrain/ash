59063f041019bef2a7418784c268df04ffa9df08
moodrain <moerain@qq.com>
2020-07-06 17:42:16 +0800

    first commit

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -4,13 +4,13 @@
     <div id="app">
         <br />
         <el-row>
-            <el-col :xs="{span:20,offset:2}" :lg="{span:6,offset:9}">
+            <el-col :span="6" :offset="9" :xs="{span:20,offset:2}">
                 <el-card>
                     <el-form>
-                        <x-input exp="model:form.email;pre:Email"></x-input>
-                        <x-input exp="model:form.name;pre:Name"></x-input>
-                        <x-input exp="model:form.password;pre:Password;type:password"></x-input>
-                        <x-input exp="model:form.rePassword;pre:RePassword;type:password"></x-input>
+                        <x-input exp="model:form.email;pre:Email" />
+                        <x-input exp="model:form.name;pre:Name" />
+                        <x-input exp="model:form.password;pre:Password;type:password" />
+                        <x-input exp="model:form.rePassword;pre:RePassword;type:password" />
                         <el-form-item>
                             <el-button @click="register">Register</el-button>
                             <el-divider direction="vertical"></el-divider>

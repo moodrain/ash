0bec061a1dea94d5ccf9192e61e1c32a64796904
moodrain <moerain@qq.com>
2020-08-19 09:56:56 +0800

    first commit

--- app/Console/Commands/ClearDemo.php
+++ app/Console/Commands/ClearDemo.php
@@ -13,20 +13,31 @@ class ClearDemo extends Command
     public function handle()
     {
         $files = [
+            // controllers
             'app/Http/Controllers/SubjectController.php',
             'app/Http/Controllers/Admin/CommentController.php',
             'app/Http/Controllers/Admin/SubjectCategoryController.php',
             'app/Http/Controllers/Admin/SubjectController.php',
+
+            // models
             'app/Models/Comment.php',
             'app/Models/Subject.php',
             'app/Models/Subject',
+
+            // views
             'resources/views/admin/subject',
             'resources/views/admin/subject-category',
             'resources/views/admin/comment',
             'resources/views/subject',
+
+            // migrations
+            'database/migrations/2020_08_05_181933_create_subjects_table.php',
+            'database/migrations/2020_08_05_182137_create_subject_categories_table.php',
+            'database/migrations/2020_08_05_182216_create_comments_table.php',
         ];
         if ($this->argument('all')) {
             $files = array_merge($files, [
+                // explorer
                 'app/Http/Controllers/Admin/ExplorerController.php',
                 'resources/views/admin/explorer',
             ]);

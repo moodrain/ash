6a90421cd9c38b5955024d88de082cdd3755dc2e
moodrain <moerain@qq.com>
2020-09-03 10:16:16 +0800

    first commit

--- database/migrations/2020_08_04_102022_create_request_logs_table.php
+++ database/migrations/2020_08_04_102022_create_request_logs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLogsTable extends Migration
+class CreateRequestLogsTable extends Migration
 {
     /**
      * Run the migrations.
